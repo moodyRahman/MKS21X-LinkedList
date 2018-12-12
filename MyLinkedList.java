8503bd451e279262695102cb317cfb16d3cbc2b9
Moody <mrahman01@stuy.edu>
2018-12-12 17:18:40 -0500

    init commit with blank files

---
+++ MyLinkedList.java
@@ -0,0 +1,15 @@
+public class MyLinkedList{
+
+private int size;
+private Node start,end;
+
+public int size(){
+	return new Integer(null);
+}
+public boolean add(int value){
+	return false;
+}
+public String toString(){
+	return "";
+}
+}
---
+++ Node.java
@@ -0,0 +1,7 @@
+public class Node{
+
+private int data;
+private Node next,prev;
+
+
+}
