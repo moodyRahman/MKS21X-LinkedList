0a86cb54bf1153481b4a8abc9df20fcb5ae734fb
Moody <mrahman01@stuy.edu>
2018-12-12 20:28:03 -0500

    only the head needs to be initilized when creating a linkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,7 +1,12 @@
 public class MyLinkedList{
 
 private int size;
-private Node start,end;
+private Node start;
+private Node end;
+
+public MyLinkedList(int inp){
+	start = new Node(inp);
+}
 
 public int size(){
 	return new Integer(null);
