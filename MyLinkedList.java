c64fa45d51aa0482d5cafc22f81c09345a4676c9
Moody <mrahman01@stuy.edu>
2018-12-12 20:37:24 -0500

    moved Node into MyLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -18,3 +18,18 @@ public String toString(){
 	return "";
 }
 }
+
+class Node{
+
+private int data;
+private Node next,prev;
+
+public Node(int data){
+	this.data = data;
+}
+
+public int get(){
+	return this.data;
+}
+
+}
